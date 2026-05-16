f8497a9d802171ea0117784908bf1d197075cf5e
stx3plus1 <stx4@ballpa.in>
2026-05-16 20:41:12 +0100

    add env vars + raise mapped windows

--- wm.c
+++ wm.c
@@ -26,6 +26,11 @@ int main(void) {
 		return 1;
 	xcb_screen_t* scr  = xcb_aux_get_screen(conn, default_screen);
 
+	// add substructure notify to root
+	uint32_t mask  = XCB_CW_EVENT_MASK;
+	uint32_t value = XCB_EVENT_MASK_SUBSTRUCTURE_NOTIFY;
+	xcb_change_window_attributes(conn, scr->root, mask, &value);
+
 	// grab alt + mouse l for move
 	xcb_grab_button(conn, 0, scr->root, 
 		XCB_EVENT_MASK_BUTTON_PRESS | XCB_EVENT_MASK_POINTER_MOTION | XCB_EVENT_MASK_BUTTON_RELEASE,
@@ -48,6 +53,10 @@ int main(void) {
 	// push init
 	xcb_flush(conn);
 
+	// set name
+	setenv("XDG_SESSION_TYPE",    "x11",    true);
+	setenv("XDG_CURRENT_DESKTOP", "zxdrag", true);
+
 	// run ~/.zxrc
 	char* home = getenv("HOME");
 	if (home) {
@@ -65,35 +74,42 @@ int main(void) {
 	int start_x,     start_y,
 		start_pos_x, start_pos_y, 
 	    start_w,     start_h;
-	xcb_window_t wnd;
+	xcb_window_t drag_wnd;
+	xcb_window_t raise_wnd;
 	xcb_generic_event_t* event;
 
 	while (1) {
 		event = xcb_wait_for_event(conn);
 		switch (event->response_type & ~0x80) {
+		// set focus to new windows
+		case XCB_MAP_NOTIFY: {
+			xcb_map_notify_event_t* map_event = (void*)event;
+			raise_wnd = map_event->window;
+			goto raise_window;
+		}
 		// raise window under cursor
 		case XCB_KEY_PRESS: {
 			xcb_key_press_event_t* key_event = (void*)event;
-
+			raise_wnd = key_event->child;
+		raise_window:
 			uint16_t mask  = XCB_CONFIG_WINDOW_STACK_MODE;
 			uint32_t value = XCB_STACK_MODE_ABOVE;
-			xcb_configure_window(conn, key_event->child, mask, &value);
-
+			xcb_configure_window(conn, raise_wnd, mask, &value);
 			xcb_set_input_focus(conn, XCB_INPUT_FOCUS_PARENT, 
-				key_event->child, XCB_CURRENT_TIME);
+				raise_wnd, XCB_CURRENT_TIME);
 		}
 		break;
 		// begin move/resize
 		case XCB_BUTTON_PRESS: {
 			xcb_button_press_event_t* button_event = (void*)event;
 
-			wnd     = button_event->child;
-			move    = (button_event->detail == XCB_BUTTON_INDEX_1);
-			start_x = button_event->root_x;
-			start_y = button_event->root_y;
+			drag_wnd = button_event->child;
+			move     = (button_event->detail == XCB_BUTTON_INDEX_1);
+			start_x  = button_event->root_x;
+			start_y  = button_event->root_y;
 
 			xcb_get_geometry_reply_t* reply = xcb_get_geometry_reply(conn, 
-				xcb_get_geometry(conn, wnd), NULL);
+				xcb_get_geometry(conn, drag_wnd), NULL);
 					
 			start_pos_x = reply->x;
 			start_pos_y = reply->y;
@@ -107,7 +123,7 @@ int main(void) {
 		// end move/resize
 		case XCB_BUTTON_RELEASE:
 			move = false;
-			wnd = XCB_NONE;
+			drag_wnd = XCB_NONE;
 			start_x = 0, start_y = 0,
 			start_w = 0, start_h = 0;
 		break;
@@ -123,7 +139,7 @@ int main(void) {
 				MAX(1, (move ? start_pos_x : start_w) + x_diff),
 				MAX(1, (move ? start_pos_y : start_h) + y_diff)
 			};
-			xcb_configure_window(conn, wnd, mask, &values);
+			xcb_configure_window(conn, drag_wnd, mask, &values);
 		}
 		break;
 		}
