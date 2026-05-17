5a124214edd57849c7ddbeff3df001e0c96bf990
stx3plus1 <stx4@ballpa.in>
2026-05-17 15:14:00 +0100

    changes...

--- wm.c
+++ wm.c
@@ -13,7 +13,8 @@
 #include <xcb/xproto.h>
 #include <xcb/xcb_aux.h>
 
-#define CONFIG "/.zxrc"
+#define INIT "/.zxrc"
+#define MOD_MASK XCB_MOD_MASK_1
 
 #define MIN(a,b) (((a)<(b))?(a):(b))
 #define MAX(a,b) (((a)>(b))?(a):(b))
@@ -26,31 +27,37 @@ int main(void) {
 	xcb_connection_t* conn = xcb_connect(NULL, &default_screen);
 	if (xcb_connection_has_error(conn) < 0 || default_screen < 0)
 		return 1;
-	xcb_screen_t* scr  = xcb_aux_get_screen(conn, default_screen);
+	xcb_screen_t* scr = xcb_aux_get_screen(conn, default_screen);
 
 	// add substructure notify to root
 	uint32_t mask  = XCB_CW_EVENT_MASK;
 	uint32_t value = XCB_EVENT_MASK_SUBSTRUCTURE_NOTIFY;
 	xcb_change_window_attributes(conn, scr->root, mask, &value);
 
-	// grab alt + mouse l for move
+	// grab JUST mouse l for move AND raise
 	xcb_grab_button(conn, 0, scr->root, 
 		XCB_EVENT_MASK_BUTTON_PRESS | XCB_EVENT_MASK_POINTER_MOTION,
 		XCB_GRAB_MODE_ASYNC, XCB_GRAB_MODE_ASYNC,
-		XCB_NONE, XCB_NONE, XCB_BUTTON_INDEX_1, XCB_MOD_MASK_1
+		XCB_NONE, XCB_NONE, XCB_BUTTON_INDEX_1, MOD_MASK
 	);
-	// ''    '' + mouse r for resize
+	// grab mask + mouse r for resize
 	xcb_grab_button(conn, 0, scr->root, 
 		XCB_EVENT_MASK_BUTTON_PRESS | XCB_EVENT_MASK_POINTER_MOTION,
 		XCB_GRAB_MODE_ASYNC, XCB_GRAB_MODE_ASYNC,
-		XCB_NONE, XCB_NONE, XCB_BUTTON_INDEX_3, XCB_MOD_MASK_1
+		XCB_NONE, XCB_NONE, XCB_BUTTON_INDEX_3, MOD_MASK
 	);
-
-	// ''    '' + f1
+	// ''     '' + f1 for raise
 	xcb_grab_key(conn, 0, scr->root, 
-		XCB_MOD_MASK_1, 67, // keycode
+		MOD_MASK, 67, // keycode
 		XCB_GRAB_MODE_ASYNC, XCB_GRAB_MODE_ASYNC
 	);
+
+	// invisible focus window
+	xcb_window_t invis_wnd = xcb_generate_id(conn);
+	xcb_create_window(conn, XCB_COPY_FROM_PARENT, invis_wnd, scr->root,
+		-1, -1, 1, 1, 0, XCB_WINDOW_CLASS_INPUT_ONLY, scr->root_visual,
+		0, NULL);
+	xcb_map_window(conn, invis_wnd);
 
 	// push init
 	xcb_flush(conn);
@@ -62,9 +69,9 @@ int main(void) {
 	// run ~/.zxrc
 	char* home = getenv("HOME");
 	if (home) {
-		char rc_path[strlen(home) + strlen(CONFIG) + 1];
+		char rc_path[strlen(home) + strlen(INIT) + 1];
 		strcpy(rc_path, home);
-		strcpy(rc_path + strlen(home), CONFIG);
+		strcpy(rc_path + strlen(home), INIT);
 
 		pid_t config_pid;
 		posix_spawnp(&config_pid, rc_path, NULL, NULL, 
@@ -80,9 +87,16 @@ int main(void) {
 	xcb_window_t raise_wnd;
 	xcb_generic_event_t* event;
 
-	while (1) {
-		event = xcb_wait_for_event(conn);
+	while ((event = xcb_wait_for_event(conn))) {
 		switch (event->response_type & ~0x80) {
+		// set focus to invisible window
+		case XCB_UNMAP_NOTIFY: {
+			xcb_unmap_notify_event_t* unmap_event = (void*)event;
+			if (unmap_event->window != raise_wnd)
+				break;
+			raise_wnd = invis_wnd;
+			goto raise_window;
+		}
 		// set focus to new windows
 		case XCB_MAP_NOTIFY: {
 			xcb_map_notify_event_t* map_event = (void*)event;
@@ -97,7 +111,7 @@ int main(void) {
 			uint16_t mask  = XCB_CONFIG_WINDOW_STACK_MODE;
 			uint32_t value = XCB_STACK_MODE_ABOVE;
 			xcb_configure_window(conn, raise_wnd, mask, &value);
-			xcb_set_input_focus(conn, XCB_INPUT_FOCUS_PARENT, 
+			xcb_set_input_focus(conn, XCB_INPUT_FOCUS_NONE, 
 				raise_wnd, XCB_CURRENT_TIME);
 		}
 		break;
@@ -128,21 +142,24 @@ int main(void) {
 			int	x_diff = motion_event->root_x - start_x;
 			int y_diff = motion_event->root_y - start_y;
 
-			uint16_t mask = move ? (XCB_CONFIG_WINDOW_X     | XCB_CONFIG_WINDOW_Y) : 
-			                       (XCB_CONFIG_WINDOW_WIDTH | XCB_CONFIG_WINDOW_HEIGHT);
+			uint16_t mask = move ? 
+				(XCB_CONFIG_WINDOW_X     | XCB_CONFIG_WINDOW_Y) :
+				(XCB_CONFIG_WINDOW_WIDTH | XCB_CONFIG_WINDOW_HEIGHT);
 			uint32_t values[2] = { 
-				MIN(MAX(0, (move ? start_pos_x : start_w) + x_diff), scr->width_in_pixels  - (move ? start_w : 0)),
-				MIN(MAX(0, (move ? start_pos_y : start_h) + y_diff), scr->height_in_pixels - (move ? start_h : 0))
+				MIN(MAX(0, (move ? start_pos_x : start_w) + x_diff),
+					scr->width_in_pixels  - (move ? start_w : 0)),
+				MIN(MAX(0, (move ? start_pos_y : start_h) + y_diff), 
+					scr->height_in_pixels - (move ? start_h : 0))
 			};
 			xcb_configure_window(conn, drag_wnd, mask, &values);
 		}
 		break;
 		}
-
-		xcb_flush(conn);
 		free(event);
+		xcb_flush(conn);
     }
 
+	xcb_destroy_window(conn, invis_wnd);
 	xcb_disconnect(conn);
 	return 0;
 }
